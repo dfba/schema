bfa16bd3c914a85385f220d2883b0ceedfc69ddb
Dave Bakker <dfba@davebakker.io>
2016-05-20 20:42:03 +0200

    Added Laravel service provider.

---
+++ src/Laravel/SchemaServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dfba\Schema\Laravel;
+
+use Dfba\Schema\Manager;
+use Dfba\Schema\Schema;
+use Illuminate\Database\Connection;
+
+class SchemaServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Manager::class, function ($app) {
+            return new Manager();
+        });
+    }
+
+    public function boot(Manager $manager, Connection $connection)
+    {
+        $this->app->singleton(Schema::class, function ($app) use($manager, $connection) {
+            return $manager->getSchema(
+                $connection->getReadPdo(),
+                $connection->getDatabaseName()
+            );
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Manager::class, Schema::class];
+    }
+
+}
